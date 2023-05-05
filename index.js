c79fbb6350f9a61260c502bc0ce2bfbfca433a6c
Vivekkumar0412 <www.vivekkumar0412@gmail.com>
2023-05-05 23:53:46 +0530

    message updated

--- index.js
+++ index.js
@@ -27,6 +27,16 @@ let products = [
     { name: "Nirma", quantity: 30, sales: 21, price: 20 },
   ];
 
+  let products2 = [
+    { name: "Liril", quantity: 10, sales: 7, price: 10 },
+    { name: "Dove", quantity: 20, sales: 9, price: 20 },
+    { name: "Pears", quantity: 35, sales: 20, price: 15 },
+    { name: "Surf", quantity: 45, sales: 22, price: 55 },
+    { name: "Ariel", quantity: 20, sales: 7, price: 40 },
+    { name: "Tide", quantity: 20, sales: 11, price: 35 },
+    { name: "Nirma", quantity: 30, sales: 21, price: 20 },
+  ];
+
   
   let table_data = (arr)=>{
     
